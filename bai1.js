94f518fa6bd0d2dc40b0a605e5e97e60f3edcb82
NgoTienHung123456789 <57626191+NgoTienHung123456789@users.noreply.github.com>
2021-04-06 22:02:26 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,15 @@
+function validTime(time) {
+    const hour = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$";
+    if (str.empty())
+  {
+     return false;
+  }
+  if(hour_match(str, pattern))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
